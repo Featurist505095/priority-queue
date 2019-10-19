fceea708cc8cafae957348949b4273da73895abc
Vladimir Shikhov <featurist@yandex.ru>
2019-10-19 19:30:54 +0400

    feat: queue part

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,31 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap();
+		//this.data = [];
 	}
 
 	push(data, priority) {
-
+		if (priority > this.maxSize) {
+			this.heap.push(data, priority);
+		}
+		else {
+			throw new Error();
+		 }
 	}
 
 	shift() {
-
+		this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.length;
 	}
 
 	isEmpty() {
-		
+		return this.heap.length === 0 ? true : false;
 	}
 }
 
